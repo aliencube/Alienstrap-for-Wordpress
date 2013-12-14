b3ea56db97585fd62446636ae0a1f5e89baeb0e7
Justin Yoo <justin.yoo@aliencube.com>
2013-12-15 01:58:20 +1100

    Updated the folder structure.

--- Alienstrap-for-Wordpress/options.php
+++ Alienstrap-for-Wordpress/options.php
@@ -8,15 +8,15 @@
 
 function optionsframework_option_name() {
 
-	// This gets the theme name from the stylesheet (lowercase and without spaces)
-	$themename = get_option( 'stylesheet' );
-	$themename = preg_replace("/\W/", "_", strtolower($themename) );
+    // This gets the theme name from the stylesheet (lowercase and without spaces)
+    $themename = get_option( 'stylesheet' );
+    $themename = preg_replace("/\W/", "_", strtolower($themename) );
 
-	$optionsframework_settings = get_option('optionsframework');
-	$optionsframework_settings['id'] = $themename;
-	update_option('optionsframework', $optionsframework_settings);
+    $optionsframework_settings = get_option('optionsframework');
+    $optionsframework_settings['id'] = $themename;
+    update_option('optionsframework', $optionsframework_settings);
 
-	// echo $themename;
+    // echo $themename;
 }
 
 /**
@@ -27,84 +27,84 @@ function optionsframework_option_name() {
 
 function optionsframework_options() {
 
-	// Test data
-	$test_array = array(
-		'one' => __('One', 'options_check'),
-		'two' => __('Two', 'options_check'),
-		'three' => __('Three', 'options_check'),
-		'four' => __('Four', 'options_check'),
-		'five' => __('Five', 'options_check')
-	);
+    // Test data
+    $test_array = array(
+        'one' => __('One', 'options_check'),
+        'two' => __('Two', 'options_check'),
+        'three' => __('Three', 'options_check'),
+        'four' => __('Four', 'options_check'),
+        'five' => __('Five', 'options_check')
+    );
 
-	// Multicheck Array
-	$multicheck_array = array(
-		'one' => __('French Toast', 'options_check'),
-		'two' => __('Pancake', 'options_check'),
-		'three' => __('Omelette', 'options_check'),
-		'four' => __('Crepe', 'options_check'),
-		'five' => __('Waffle', 'options_check')
-	);
+    // Multicheck Array
+    $multicheck_array = array(
+        'one' => __('French Toast', 'options_check'),
+        'two' => __('Pancake', 'options_check'),
+        'three' => __('Omelette', 'options_check'),
+        'four' => __('Crepe', 'options_check'),
+        'five' => __('Waffle', 'options_check')
+    );
 
-	// Multicheck Defaults
-	$multicheck_defaults = array(
-		'one' => '1',
-		'five' => '1'
-	);
+    // Multicheck Defaults
+    $multicheck_defaults = array(
+        'one' => '1',
+        'five' => '1'
+    );
 
-	// Background Defaults
-	$background_defaults = array(
-		'color' => '',
-		'image' => '',
-		'repeat' => 'repeat',
-		'position' => 'top center',
-		'attachment'=>'scroll' );
+    // Background Defaults
+    $background_defaults = array(
+        'color' => '',
+        'image' => '',
+        'repeat' => 'repeat',
+        'position' => 'top center',
+        'attachment'=>'scroll' );
 
-	// Typography Defaults
-	$typography_defaults = array(
-		'size' => '15px',
-		'face' => 'georgia',
-		'style' => 'bold',
-		'color' => '#bada55' );
+    // Typography Defaults
+    $typography_defaults = array(
+        'size' => '15px',
+        'face' => 'georgia',
+        'style' => 'bold',
+        'color' => '#bada55' );
 
-	// Typography Options
-	$typography_options = array(
-		'sizes' => array( '6','12','14','16','20' ),
-		'faces' => array( 'Helvetica Neue' => 'Helvetica Neue','Arial' => 'Arial' ),
-		'styles' => array( 'normal' => 'Normal','bold' => 'Bold' ),
-		'color' => false
-	);
+    // Typography Options
+    $typography_options = array(
+        'sizes' => array( '6','12','14','16','20' ),
+        'faces' => array( 'Helvetica Neue' => 'Helvetica Neue','Arial' => 'Arial' ),
+        'styles' => array( 'normal' => 'Normal','bold' => 'Bold' ),
+        'color' => false
+    );
     
     $fonts_array = array(
         "Nanum Gothic"  => "Nanum Gothic",
         "Nanum Myeonjo"  => "Nanum Myeonjo"
     );
 
-	// Pull all the categories into an array
-	$options_categories = array();
-	$options_categories_obj = get_categories();
-	foreach ($options_categories_obj as $category) {
-		$options_categories[$category->cat_ID] = $category->cat_name;
-	}
+    // Pull all the categories into an array
+    $options_categories = array();
+    $options_categories_obj = get_categories();
+    foreach ($options_categories_obj as $category) {
+        $options_categories[$category->cat_ID] = $category->cat_name;
+    }
 
-	// Pull all tags into an array
-	$options_tags = array();
-	$options_tags_obj = get_tags();
-	foreach ( $options_tags_obj as $tag ) {
-		$options_tags[$tag->term_id] = $tag->name;
-	}
+    // Pull all tags into an array
+    $options_tags = array();
+    $options_tags_obj = get_tags();
+    foreach ( $options_tags_obj as $tag ) {
+        $options_tags[$tag->term_id] = $tag->name;
+    }
 
-	// Pull all the pages into an array
-	$options_pages = array();
-	$options_pages_obj = get_pages('sort_column=post_parent,menu_order');
-	$options_pages[''] = 'Select a page:';
-	foreach ($options_pages_obj as $page) {
-		$options_pages[$page->ID] = $page->post_title;
-	}
+    // Pull all the pages into an array
+    $options_pages = array();
+    $options_pages_obj = get_pages('sort_column=post_parent,menu_order');
+    $options_pages[''] = 'Select a page:';
+    foreach ($options_pages_obj as $page) {
+        $options_pages[$page->ID] = $page->post_title;
+    }
 
-	// If using image radio buttons, define a directory path
-	$imagepath =  get_template_directory_uri() . '/images/';
+    // If using image radio buttons, define a directory path
+    $imagepath =  get_template_directory_uri() . '/images/';
 
-	$options = array();
+    $options = array();
 
     $options[] = array(
         "name"  => "Logo",
@@ -332,5 +332,5 @@ function optionsframework_options() {
 //		'type' => 'editor',
 //		'settings' => $wp_editor_settings );
 
-	return $options;
+    return $options;
 }
