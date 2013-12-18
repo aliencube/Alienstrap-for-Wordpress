753b845eea0896ebeac0a0841dbcc6e6e78107b3
Justin Yoo <justin.yoo@aliencube.com>
2013-12-18 23:42:37 +1100

    Added favion path.

--- Alienstrap-for-Wordpress/header.php
+++ Alienstrap-for-Wordpress/header.php
@@ -40,6 +40,16 @@
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( "pingback_url" ); ?>" />
 
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
+
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/bootstrap/bootstrap.min.css" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/bootstrap/bootstrap-theme.min.css" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/main.css" />
