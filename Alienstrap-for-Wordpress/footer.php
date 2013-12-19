64d86a6d6dc0876bc1941039808a93d32b074217
Justin Yoo <justin.yoo@aliencube.com>
2013-12-19 22:14:27 +1100

    Removed fake Google Analytics script block.

--- Alienstrap-for-Wordpress/footer.php
+++ Alienstrap-for-Wordpress/footer.php
@@ -57,17 +57,5 @@
     <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/scripts/main.js?v=<?php echo get_last_modified_date("/scripts/main.js"); ?>"></script>
 
     <?php wp_footer(); ?>
-
-    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-    <script type="text/javascript">
-        (function (b, o, i, l, e, r) {
-            b.GoogleAnalyticsObject = l; b[l] || (b[l] =
-                function () { (b[l].q = b[l].q || []).push(arguments) }); b[l].l = +new Date;
-            e = o.createElement(i); r = o.getElementsByTagName(i)[0];
-            e.src = '//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e, r)
-        }(window, document, 'script', 'ga'));
-        ga('create', 'UA-XXXXX-X'); ga('send', 'pageview');
-    </script>
 </body>
 </html>
